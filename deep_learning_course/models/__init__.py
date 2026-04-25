287bfa6f19adb4d79dccc174648fcdd993ae6b6a
mehtimans <mmhdimansouri@gmail.com>
2026-04-25 05:10:58 +0330

    Add CNN Network to Models

--- deep_learning_course/models/__init__.py
+++ deep_learning_course/models/__init__.py
@@ -11,6 +11,9 @@ or research purposes.
 """
 
 from .MLP_Network import MLPNetwork
+from .Autoencoder_Network import AutoEncoderNetwork
+from .CNN_Network import CNNNetwork
 from .LSTM_Network import LSTMNetwork
 from .GRU_Network import GRUNetwork
-from .Autoencoder_Network import AutoEncoderNetwork
+
+
